ade0fa330c27833ca798faadedb75a941c16b050
HookSandeer <hooksander@proton.me>
2022-08-31 12:49:01 +0200

    Final version

--- script.py
+++ script.py
@@ -9,22 +9,36 @@ def userLink():
     downloadFile = Playlist(var)
     return downloadFile
 
+def valide():
+    user = input("Voulez vous télécharger cette playlist ? [Y/o]\n=> ").lower()
+    if user == 'y' :
+        return True
+    else :
+        return False
+
 def download(playlist):
     playlistLen = len(playlist.videos)
     print("Téléchargement de laplaylist : {}\n Nombre de vidéos : {}".format(playlist.title, playlistLen))
     count = 0
-    for video in playlist.videos :
-        count += 1
-        print("Téléchargement de la vidéo {}/{}".format(count, playlistLen))
-        file = video.streams.filter(only_audio=True).download()
-        base, ext = os.path.splitext(file)
-        newFile = base + ".mp3"
-        os.rename(file, newFile)
-        print("{} a finit de télécharger.".format(video.title))
-    print("Téléchargement terminé.")
+    if valide() :
+        for video in playlist.videos :
+            count += 1
+            print("Téléchargement de la vidéo {}/{}".format(count, playlistLen))
+            audioFile = video.streams.filter(only_audio=True).first()
+            file = audioFile.download()
+            base, ext = os.path.splitext(file)
+            newFile = base + ".mp3"
+            os.rename(file, newFile)
+            print("{} a finit de télécharger.".format(video.title))
+        print("Téléchargement terminé.")
+    else :
+        input("Appuyer sur entrer pour quitter, téléchargement annulé")
+
+
     
 
     
 if __name__ ==  '__main__' :
     download(userLink())
     input = ("Appuyer sur Entrer pour quiter...")
+    
