4cd84cc07d30a1c44d67c3d10b8ccb2d7503b2cc
HookSandeer <hooksander@proton.me>
2022-08-31 16:44:01 +0200

    Instalation automatique

---
+++ launche.sh
@@ -0,0 +1,7 @@
+echo "Téléchargeur de playlist YouTube par HookSander"
+sleep 1
+echo "Lancement du script ...."
+sleep 1
+python3 ./script.py
+echo "Appuyer sur entrer pour continuer"
+read input
---
+++ module.sh
@@ -0,0 +1,4 @@
+echo "Installation du module Pytube"
+pip install Pytube
+echo "Module Pytube installé, appuyer sur entrer pour quitter."
+read input
