01cec92b63c844029d1d6f3fdb5151c08d7fd21e
HookSandeer <hooksander@proton.me>
2022-09-01 00:41:05 +0200

    Setup Fill dor .dmg package

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+#! By HookSander
+
+from setuptools import setup
+
+APP = ['script.py']
+OPTIONS = {
+    'argv_emulation': True,
+}
+
+setup(
+    app=APP
+    options={'py2app' : OPTIONS},
+    setup_requires=['py2app']
+)
